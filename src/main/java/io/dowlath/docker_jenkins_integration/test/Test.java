d27c27594735b2163093e9d480640c1a2169280e
DowlathBashaG <dowlath_g@yahoo.com>
2024-05-02 10:01:30 +0530

    second commit

---
+++ src/main/java/io/dowlath/docker_jenkins_integration/test/Test.java
@@ -0,0 +1,8 @@
+package io.dowlath.docker_jenkins_integration.test;
+
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("Welcome to jenkins and docker integration...");
+    }
+
+}
